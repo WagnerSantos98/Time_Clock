4c38c46b11ad7ab728d83dd9707dc331e8d644c8
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-05 15:45:08 -0300

    refactor: atualizando interface do usuário

--- index.php
+++ index.php
@@ -11,25 +11,62 @@ session_start();
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.2.js"></script>
+    <script src="assets/js/app.js"></script>
     <title>Relógio de Ponto</title>
 </head>
 <body>
-    <h1>Registar Ponto</h1>
 
-    <?php
-        if(isset($_SESSION['msg'])){
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
-        }
-  ?>
+  <div class="container">
+    <div class="card-panel grey ligthen-2">
+        <div class="row">
+            <h1>Relógio de Ponto</h1>
+            <form class="col s12">
+                <div class="col m4">
+                    <div class="card-panel teal">
+                        <output id="horario" class="time"><?php echo date("d/m/Y H:i:s"); ?></output>
+                    </div>
+                </div>
 
-    <!--Seletor horário-->
-    <p id="horario"><?php echo date("d/m/Y H:i:s"); ?></p>
+                <div class="col m4">
+                    <div class="card-panel teal">
+                        <label for="">Colaborador</label>
+                        <select name="" id="">
+                            <option value=""><?php
+                            if(isset($_SESSION['nome_colaborador'])){
+                                echo $_SESSION['nome_colaborador'];
+                            }
+                            ?></option>
+                        </select>
+                        <a href="actions/register_point.php" class="btn btn-primary">Entrada</a>
+                        <a href="actions/register_point.php" class="btn btn-primary">Saída</a>
+                        <div class="alert alert-primary" role="alert">
+                        <?php
+                                if(isset($_SESSION['msg'])){
+                                    echo $_SESSION['msg'];
+                                    unset($_SESSION['msg']);
+                                }
+                        ?>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+  </div>
 
-    <a href="actions/register_point.php">Registar</a>
+    
 
-    <script src="assets/js/app.js"></script>
+    <!--<a href="actions/register_point.php">Registar</a>-->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+</script>
 </body>
 </html>
