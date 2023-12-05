11e492a2f260f07bd098fca779fd1dc5f558270d
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-05 15:20:20 -0300

    style: importação de fonte

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Relógio de Ponto</title>
 </head>
 <body>
@@ -25,6 +30,6 @@ session_start();
 
     <a href="actions/register_point.php">Registar</a>
 
-    <script src="js/app.js"></script>
+    <script src="assets/js/app.js"></script>
 </body>
 </html>
